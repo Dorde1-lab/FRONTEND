4ae51a30f8154f2b4bcdebf7d6effe3e34ca2e5d
Dorde <djordjel4444@yahoo.com>
2024-01-16 20:12:51 +0100

    .

---
+++ JavaScript/javascript.js
@@ -0,0 +1,29 @@
+function sum (a, b)
+{
+ console.log(a + b);
+}
+sum (1, 2);
+
+var testvar= null;
+alert (testvar); //shows null
+alert (typeof testvar); //shows number 
+
+var first = 1;
+console.log( first, "first");
+
+// this is a string
+
+var age = "24";
+console.log(age, "age");
+
+// this is a number 
+
+var age = 24;
+console.log(age, "age");
+
+// This is a boolean
+
+var knowsHtml = true;
+var knowJS = false;
+console.log(knowsHtml "knowsHtml");
+console.log(knowJS "knowJS");
