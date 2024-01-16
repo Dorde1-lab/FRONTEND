c77626073b543bf7b3dfafb971f03f8a9d46432e
Dorde <djordjel4444@yahoo.com>
2024-01-16 20:50:44 +0100

    .

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -3,10 +3,13 @@ function sum (a, b)
  console.log(a + b);
 }
 sum (1, 2);
+// Provera koji je tip varijable ... IMPORTANT !!!
 
 var testvar= null;
 alert (testvar); //shows null
 alert (typeof testvar); //shows number 
+
+// Number 
 
 var first = 1;
 console.log( first, "first");
@@ -27,3 +30,67 @@ var knowsHtml = true;
 var knowJS = false;
 console.log(knowsHtml, "knowsHtml");
 console.log(knowJS, "knowJS");
+
+// arithmetic operators
+
+var a = 5;
+var b = 2;
+console.log (a + b);
+console.log (a - b);
+console.log (a * b);
+console.log (a / b);
+console.log (a % b);
+console.log (a++);
+console.log (a);
+// u ovom se a uvecava za 1 u odnosu na prethodni rezultat
+console.log (a++);
+
+console.log (++a);
+
+c = 5;
+
+console.log (c++);
+
+console.log ( c + 9 );
+console.log (++c);
+
+//comparison operators
+var x = 5;
+var y = 2;
+console.log(x == y); //false
+console.log(x != y); //true
+console.log(x > y); //true
+console.log(x < y); //false
+console.log(x >= y); //true
+console.log(x <= y); //false
+
+
+//comparison operators
+var x = 5;
+var y = 2;
+console.log(x == y); //false
+console.log(x != y); //true
+console.log(x > y); //true
+console.log(x < y); //false
+console.log(x >= y); //true
+console.log(x <= y); //false
+ 
+//asignment operators
+var x = 5;
+x += 2; // 7
+console.log(x);
+x -= 2; // 5
+console.log(x);
+x *= 2; // 10
+console.log(x);
+x /= 2; // 5
+console.log(x);
+x %= 2; // 1
+console.log(x);
+
+
+
+
+  var a = 2;
+  var b = 2;
+   c = a*b ;
