80e553136fe967391ffd4c99a192a087a1564c44
Dorde <djordjel4444@yahoo.com>
2024-01-24 21:09:51 +0100

    .

---
+++ JAVASCRIPT/Vezbe/homework2.js
@@ -0,0 +1,98 @@
+// let sredstva = 500;
+
+
+// function bankomat () {
+ 
+//     let unos = prompt ("Unesi zeljnu sumu novca:");
+
+//     let iznos = parseInt(unos);
+
+// if (iznos > sredstva) {
+//     console.log ("Nemate dovoljno para na racunu.")
+// } else {
+//    console.log (" Bice isplaceno " + iznos + " evra ")}
+// }
+// bankomat ()
+
+
+// <<<<<<< WITH RETURN >>>>>>>>
+
+
+// let sredstva = 500;
+
+
+// function bankomat () {
+ 
+//     let unos = prompt ("Unesi zeljnu sumu novca:");
+
+//     let iznos = parseInt(unos);
+
+// if (iznos > sredstva) {
+//     console.log ("Nemate dovoljno para na racunu.")
+// } else { return iznos;}
+// }
+
+// let rezultat = bankomat ();
+// console.log (" Bice isplaceno ", rezultat);
+
+
+
+//      <<<<< SWITCH >>>>>
+
+
+// let mesec = prompt ("Unesite ime meseca u godini:");
+
+// switch (mesec) {
+
+//     case 'januar' :
+//         console.log (" Mesec ", mesec, " ima 31 dan. ");
+//     break;
+
+//     case 'februar':
+//         console.log (" Mesec ", mesec, " ima 29 dana. ");
+//     break;
+
+//     case 'mart':
+//         console.log (" Mesec ", mesec, " ima 31 dana. ");
+//     break;
+
+//     case 'april':
+//         console.log (" Mesec ", mesec, " ima 30 dana. ");
+//     break;
+
+//     case 'maj':
+//         console.log (" Mesec ", mesec, " ima 31 dana. ");
+//     break;
+
+//     case 'jun':
+//         console.log (" Mesec ", mesec, " ima 30 dana. ");
+//     break;
+
+//     case 'jul':
+//         console.log (" Mesec ", mesec, " ima 31 dana. ");
+//     break;
+
+//     case 'avgust':
+//         console.log (" Mesec ", mesec, " ima 31 dana. ");
+//     break;
+
+//     case 'septembar':
+//         console.log (" Mesec ", mesec, " ima 30 dana. ");
+//     break;
+
+//     case 'oktobar':
+//         console.log (" Mesec ", mesec, " ima 31 dana. ");
+//     break;
+
+//     case 'novembar':
+//         console.log (" Mesec ", mesec, " ima 30 dana. ");
+//     break;
+
+//     case 'decembar':
+//         console.log (" Mesec ", mesec, " ima 31 dana. ");
+//     break;
+
+//     default :
+//         console.log ("Unesite validanu rec!");
+//     break;
+// }
--- JAVASCRIPT/Vezbe/important1.js
+++ JAVASCRIPT/Vezbe/important1.js
@@ -1,27 +1,5 @@
 
 
-// function temperatura (celzijus) {
-//    let farenhajt = celzijus * 1.8 + 32;
-//    return (farenhajt)
-// }
-// let farenhajt = temperatura(50)
-
-// console.log (farenhajt);
-
-// celzijus = prompt ("upisi temperaturu u celzijusu");
-
-// tempcelzijus = parseInt(celzijus);
-     
-
-// function calculateAge (birthyear, currentyear) {
-//        let godina = currentyear - birthyear;
-//        return godina; 
-// }
-// console.log (calculateAge (1995, 2024));
-
-
-
-
 
 
 
