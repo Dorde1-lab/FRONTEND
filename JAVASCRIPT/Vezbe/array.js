2cbd219934cc1caccd0391008eeeb9744862493c
Dorde <djordjel4444@yahoo.com>
2024-01-25 17:36:08 +0100

    .

---
+++ JAVASCRIPT/Vezbe/array.js
@@ -0,0 +1,57 @@
+
+// let days = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
+
+
+// ...........................................................................
+
+
+
+// var unos = prompt ("Unesi godinu rodjenja:");
+
+// var godina = parseInt(unos);
+
+// if (!isNaN(godina)) {
+
+//     var znak = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"];
+
+//     var ostatak = (unos - 4) % 12;
+//     console.log (znak[ostatak]);}
+//     else {
+//         console.log ("Nista");
+//     }
+
+
+// ...........................................................................
+
+
+    // Provera koliki je niz:
+
+
+    // const days = [ 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun' ];
+
+    // console.log (" prikazi : ", days[2]);
+    // console.log (" duzina niza je: ", days.length);
+
+
+// ...........................................................................
+
+
+    //  Zamena vrednosti jednog u nizu: 
+
+    // const days = [ 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun' ];
+
+    // days[2] = "Neki drugi dan:";
+    // console.log (days[2]);
+
+
+// ...........................................................................
+
+
+    // Promena vrednosti na poslednjoj poziciji:
+
+
+        const days = [ 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun' ];
+
+        days[days.length = 1] = 'petar';
+        console.log (days[6]);
+
