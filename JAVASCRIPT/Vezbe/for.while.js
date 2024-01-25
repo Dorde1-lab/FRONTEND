1248192f6f1cdfd4909ab5c8e6ebc4f6f393c3e2
Dorde <djordjel4444@yahoo.com>
2024-01-25 17:51:45 +0100

    .

---
+++ JAVASCRIPT/Vezbe/for.while.js
@@ -0,0 +1,13 @@
+
+
+// ...........................................................................
+
+let days = [ 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun' ];
+
+let daycounter = 0;
+
+while (daycounter < days.length) {
+console.log (" Today is ", days[daycounter]);
+daycounter++;
+
+}
