99a4d92b9f8708d04a8d466fbd9a4392be5fa108
Dorde1-lab <djordjel4444@yahoo.com>
2024-01-27 23:05:32 +0100

    .

--- JAVASCRIPT/Vezbe/homework33.js
+++ JAVASCRIPT/Vezbe/homework33.js
@@ -60,30 +60,30 @@
 
 
 
-// let niz = [1, 2, 3, 4, 5];
-// let index = 0;
+let niz = [1, 2, 3, 4, 5];
+let index = 0;
 
-// function validateNumber (niz){
+function validateNumber (niz){
 
 
-//     while ( index < niz.length){
-//         if ( isNaN(niz[index]) ) {
+    while ( index < niz.length){
+        if ( isNaN(niz[index]) ) {
 
-//             console.log ("Neki od brojeva nije validan.");
-//         } else { switch(index){
-//             case 0:
-//                 console.log (niz[0]);
-//                 break;
+            console.log ("Neki od brojeva nije validan.");
+        } else { switch(index){
+            case 0:
+                console.log (niz[0]);
+                break;
 
-//         }         
-//         }
+        }         
+        }
 
-//         }
+        }
 
      
-//     }
+    }
 
-//     validateNumber (niz)
+    validateNumber (niz)
 
 
 
