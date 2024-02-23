2c2867783736bbc5e0f25043ef1a81dd098cf266
Dorde1-lab <djordjel4444@yahoo.com>
2024-02-23 20:46:47 +0100

    .

--- JAVASCRIPT/Vezbe/jQuery.js
+++ JAVASCRIPT/Vezbe/jQuery.js
@@ -1,21 +1,115 @@
-let allElements = $("*"); 
-let byIdElement = $("#mainTitle"); 
-let byClassElements = $(".innerWrapper"); 
-let byTagNameElements = $("p"); 
-let wrapperAndInnerWrapper = $(".wrapper,.innerWrapper");
-let firstParagraph = $("p:first"); 
-let lastParagraph = $("p:last"); 
-let firstChildHeaders = $("h3:first-child");
-let lastChildParagraphs = $("p:last-child"); 
-let secondChildParagraphs = $("p:nth-child(2)"); 
+// let byIdElement = $("#mainTitle").text(); 
 
-// console.log(allElements);
-console.log(byIdElement);
-console.log(byClassElements);
-console.log(byTagNameElements);
-console.log(wrapperAndInnerWrapper);
-console.log(firstParagraph);
-console.log(lastParagraph);
-console.log(firstChildHeaders);
-console.log(lastChildParagraphs);
-console.log(secondChildParagraphs);
+// // console.log(allElements);
+// console.log(byIdElement);
+
+
+
+
+// let allElements = $("*"); 
+// // gets all elements
+// let byIdElement = $("#mainTitle"); 
+// // gets element by id
+// let byClassElements = $(".innerWrapper");
+//  // gets elements by class name
+// let byTagNameElements = $("p");
+//  // gets elements by tag name
+// let wrapperAndInnerWrapper = $(".wrapper,.innerWrapper");
+//  // gets elements from multiple classes
+// let firstParagraph = $("p:first");
+//  // gets the first p element
+// let lastParagraph = $("p:last"); 
+// // gets the last p element
+// let firstChildHeaders = $("h3:first-child");
+//  // gets all the first h3 elements in their parents
+// let lastChildParagraphs = $("p:last-child"); 
+// // gets all the last p elements in their parents
+// let secondChildParagraphs = $("p:nth-child(2)"); 
+// // gets all the second place p elements in their parents
+// console.log(firstParagraph.text());
+
+
+// // selecting
+
+// allElements.first();
+// allElements.last();
+// // Find elements in JQuery array
+// allElements.find(".wrapper").find("p").last();
+// // Get DOM element from JQuery array
+// allElements[0];
+// // Get all DOM elements from a JQuery array
+// byClassElements.get();
+// // Get next or previous element
+// firstParagraph.next();
+// firstParagraph.prev();
+
+
+
+//  prva vezba...................
+
+// $(document).ready(function(){
+//  let div = $("#firstWrapper");
+//  let par = $("p").first();
+//  let h1 = $("h1").last();
+//  let h3 = $("h3:first");
+//  let button = $("button");
+// })
+
+
+
+
+// $("input").first().val();// get value from input
+// secondChildParagraphs.hide();// hide elements
+// secondChildParagraphs.show();// show elements
+// byClassElements.first().html();// get innerHTML
+// byClassElements.first().html(`<h1> This is a new h1! </h1>`);// set innerHTML
+// byTagNameElements.last().text();// get text
+// byTagNameElements.last().text("This is a new text");// set text
+// byTagNameElements.last().css("color");// getting CSS
+// byTagNameElements.last().css("color", "red");// Changing CSS
+// $("h1").first().after("<p>new p tag after the h1</p>");
+// $("h1").first().before("<p>new p tag before the h1</p>");
+
+
+
+
+// vezba
+
+
+// $(document).ready(function(){
+//  $("h1").first().text("JQuery is awesome");
+//  $("h1").first().after("<h2> Novi header </h2>");
+//   $("button").css("background-color", "red");
+// })
+
+
+// eventListener 
+
+
+
+// let button = $("button").first();
+// // simple on event click
+// button.on("click", function(){
+//   console.log("Hello");
+// })
+// // Even simpler click event
+// button.click(function(e){
+//   console.log("HELLO AGAIN");})
+
+
+
+// $(document).ready(function(){
+// $("button").first().click(function(){
+//     $("#secondWrapper").hide();
+//     $("p").css("color", "green");
+//     $("button").first().text("Don't click me");
+// });
+// });
+
+$(document).ready(function(){
+let pera = $("#secondWrapper").find("p").get();
+ 
+pera.forEach(function(element, index){
+    $(element).css("color", "red");
+})
+});
---
+++ JS Advanced/class2.js
@@ -0,0 +1,145 @@
+
+
+// let input1 = document.getElementById("input1");
+// let input2 = document.getElementById("input2");
+// let input3 = document.getElementById("input3");
+// let button = document.getElementById("btn");
+// let div = document.getElementById("div");
+
+
+// function myFunction(){
+// let input1Value = input1.value;
+// let input2Value = input2.value;
+// let input3Value = input3.value;
+
+// let header = `<h1>${input1Value} ${input2Value} ${input3Value}</h1>`;
+
+// // let header = document.createElement("h1");                                        ....... ovo je drugi nacin sa document.createElement()
+// // header.textContent = `${input1Value} ${input2Value} ${input3Value}`;              .......  ovo je drugi nacin sa document.createElement()
+
+
+// div.appendChild(header);
+// }
+
+
+
+
+
+
+// let list = document.getElementById("list");
+
+// let niz = ["Milica", "Sava", "Djordje", "Zarko", "Sava"];
+
+// function newList(){
+//     for (let ime of niz){
+//         list.innerHTML += `<li> ${ime}  </li>`
+//     }
+// }
+
+// const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+ 
+// function fillList() {
+//   let list = document.getElementById("name-list");
+//   for (let i = 0; i < names.length; i++) {
+//     let item = document.createElement("li");
+//     item.textContent = names[i];
+//     list.appendChild(item);
+//   }
+// }
+
+
+
+
+
+
+
+// function getRandomNumber() {
+//   // 0 - 255
+//   return Math.floor(Math.random() * 256);                                                   da odradimo opet ???
+// }
+
+// function getColor() {
+//   // rgb(123, 22, 33)
+//   return `rgb(${getRandomNumber()}, ${getRandomNumber()},  ${getRandomNumber()})`;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// Create 3 inputs:
+// Color
+// FontSize
+// Items
+// Create a button for generating unordered lists
+// When the button is clicked generate a new ul element with the color, font size, and items from the inputs
+// Items should be added separated by comma (,) in the input
+
+
+
+
+const generateBtn = document.getElementById("generateBtn");
+const colorInput = document.getElementById("colorInput");
+const fontSizeInput = document.getElementById("fontSizeInput");
+const itemsInput = document.getElementById("itemsInput");
+
+function onGenerate() {
+  const list = document.createElement("ul");
+  const itemsArray = itemsInput.value.split(","); // ['aa', 'bbb', 'ccc'];
+  const color = colorInput.value;
+  const fs = fontSizeInput.value;
+  let isFormValid = true;
+
+  if (!colorInput.value) {
+    console.warn("Color is required");
+    isFormValid = false;
+  }
+  if (!fontSizeInput.value || isNaN(fontSizeInput.value)) {
+    console.warn("font size is required and must be a number");
+    isFormValid = false;
+  }
+  if (!itemsInput.value) {
+    console.warn("items are required");
+    isFormValid = false;
+  }
+
+  if (isFormValid) {
+    itemsArray.forEach(function (element) {
+      const li = document.createElement("li");
+      li.style.color = color;
+      li.style.fontSize = `${fs}px`;
+      li.innerText = element;
+      list.appendChild(li);
+    });
+
+    document.body.appendChild(list);
+
+    colorInput.value = "";
+    fontSizeInput.value = "";
+    itemsInput.value = "";
+  }
+}
+
+// function onGenerate2() {
+//   let list = '<ul>';
+//   const itemsArray = itemsInput.value.split(',');// ['aa', 'bbb', 'ccc'];
+//   console.log(itemsArray);
+
+//   itemsArray.forEach(function(element){
+//     list += `<li>${element}</li>`;
+//   });
+
+//   list += '</ul>';
+
+//   console.log(list);
+
+//   document.body.innerHTML += list;
+// }
+
+// generateBtn.addEventListener("click", onGenerate);
