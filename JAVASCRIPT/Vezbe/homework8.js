609a33d2cc4cd27aee0baeefb2f8786b156dce0d
Dorde1-lab <djordjel4444@yahoo.com>
2024-02-25 21:41:03 +0100

    .

--- JAVASCRIPT/Vezbe/homework8.js
+++ JAVASCRIPT/Vezbe/homework8.js
@@ -1,17 +1,50 @@
 // domaci 1
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    let button = $("#btn");
-    let h1 = $("#h1");
+//     let button = $("#btn");
+//     let h1 = $("#h1");
 
-    button.click(function(){
+//     button.click(function(){
 
-        let input = $("#input").val();
-        h1.text(`Hello there ${input}`);
+//         let input = $("#input").val();
+//         h1.text(`Hello there ${input}`);
     
-        $("#input").val('');
-    })
-})
+//         $("#input").val('');
+//     })
+// })
 
+
+
+// domaci 2
+
+
+
+// $(document).ready(function(){
+
+//     let button = $("#btn");
+//     let h3 = $("#h3");
+
+
+//     button.click(function(){
+
+//         let text = $("#text").val();
+//         let color = $("#color").val();
+//         let isValid = true;
+
+//         if (!text || !color || !isNaN(color) || !isNaN(text)){
+//             h3.text("input invalid");
+//             isValid = false;
+//         }
+
+//         if (isValid){
+//         let newH1 = $("<h1>").text(text).css("color", color);
+
+//         button.after(newH1);
+//     }
+
+        
+//     })
+
+// })
