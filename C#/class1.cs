f20d42dd3136043cb0f6026b4c2b6fb6960c2107
Dorde1-lab <djordjel4444@yahoo.com>
2024-04-11 19:14:56 +0200

    .

---
+++ C#/class1.cs
@@ -0,0 +1,48 @@
+
+namespace PEra
+{
+   internal class Program
+   {
+    static void Main(string[]args){
+
+
+
+
+
+// console.WriteLine($"Pera i zika {mara}")
+
+
+// string firstName = "Mara";
+// string lastName = "Pera";
+// int age = 23;
+
+// console.WriteLine("What's your name?");
+// firstName = console.ReadLine();
+
+// console.WriteLine("vase prezime je ?");
+// lastName = console.ReadLine();
+
+// console.WriteLine("koliko imas godina");
+// int age = int.Parse(console.ReadLine());
+
+
+
+
+
+// ............................ metode
+
+
+console.WriteLine("Koliko imas godina?");
+string godiste = console.ReadLine;
+
+int age = int.Parse(godiste);
+int ageinput = Convert.ToInt(godiste);
+
+// ......................toDouble
+
+console.WriteLine("What is a value of Pi number?");
+double numberPi = Convert.ToInt(console.ReadLine());
+ console.WriteLine(numberPi);
+    }
+   }    
+}
